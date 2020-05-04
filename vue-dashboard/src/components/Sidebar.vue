a58255df933d1fa877eae63d16cd6f6af8b98b23
Raghav Raman <rraman@covermymeds.com>
2020-05-03 20:44:18 -0400

    makng sidebar component

--- vue-dashboard/src/components/Sidebar.vue
+++ vue-dashboard/src/components/Sidebar.vue
@@ -1,10 +1,5 @@
 <template>
   <div class="sidebar" data-image="../../static/img/sidebar-5.jpg" data-color="blue">
-    <!--
-        Tip 1: You can change the color of the sidebar using: data-color="purple | blue | green | orange | red"
-
-        Tip 2: you can also add an image using data-image tag
-    -->
     <div class="sidebar-wrapper">
       <div class="logo">
         <a href="http://www.creative-tim.com" class="simple-text">
@@ -12,58 +7,25 @@
         </a>
       </div>
       <ul class="nav">
-        <li :class="{'nav-item active': active =='dashboard'}">
-          <router-link class="nav-link" to="/dashboard">
-            <i class="nc-icon nc-chart-pie-35"></i>
-            <p>Dashboard</p>
-          </router-link>
-        </li>
-        <li :class="{'nav-item active': active == 'user'}">
-          <router-link class="nav-link" to="./user">
-            <i class="nc-icon nc-circle-09"></i>
-            <p>User Profile</p>
-          </router-link>
-        </li>
-        <li :class="{'nav-item active': active == 'table'}">
-          <router-link class="nav-link" to="./table">
-            <i class="nc-icon nc-notes"></i>
-            <p>Table List</p>
-          </router-link>
-        </li>
-        <li :class="{'nav-item active': active == 'typography'}">
-          <router-link class="nav-link" to="./typography">
-            <i class="nc-icon nc-paper-2"></i>
-            <p>Typography</p>
-          </router-link>
-        </li>
-        <li :class="{'nav-item active': active == 'icons'}">
-          <router-link class="nav-link" to="./icons">
-            <i class="nc-icon nc-atom"></i>
-            <p>Icons</p>
-          </router-link>
-        </li>
-        <li :class="{'nav-item active': active == 'maps'}">
-          <router-link class="nav-link" to="./maps">
-            <i class="nc-icon nc-pin-3"></i>
-            <p>Maps</p>
-          </router-link>
-        </li>
-        <li :class="{'nav-item active': active == 'notifications'}">
-          <router-link class="nav-link" to="./notifications">
-            <i class="nc-icon nc-bell-55"></i>
-            <p>Notifications</p>
-          </router-link>
-        </li>
+        <SidebarItem url="/dashboard" title="dashboard" icon="nc-chart-pie-35" />
+        <SidebarItem url="/user" title="User Profile" icon="nc-circle-09" />
+        <SidebarItem url="/table" title="Table List" icon="nc-notes" />
+        <SidebarItem url="/typography" title="typography" icon="nc-paper-2" />
+        <SidebarItem url="/icons" title="icons" icon="nc-atom" />
+        <SidebarItem url="/maps" title="maps" icon="nc-pin-3" />
+        <SidebarItem url="/notifications" title="notifications" icon="nc-bell-55" />
       </ul>
     </div>
   </div>
 </template>
 <script>
+import SidebarItem from '@/components/Sidebar/SidebarItem'
 export default {
   name: 'Sidebar',
   props: {
     active: String
-  }
+  },
+  components: { SidebarItem }
 }
 </script>
 <style>
---
+++ vue-dashboard/src/components/Sidebar/SidebarItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <li>
+    <router-link class="nav-link" :to="url">
+      <i class="nc-icon" :class="icon"></i>
+      <p>{{ title }}</p>
+    </router-link>
+  </li>
+</template>
+<script>
+export default {
+  name: 'SidebarItem',
+  props: {
+    url: { type: String },
+    title: { type: String },
+    icon: { type: String }
+  }
+}
+</script>
